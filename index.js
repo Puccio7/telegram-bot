30d64a5295d60260d3712919e69e36057a8000f6
Puccio7 <andreapucci2000@gmail.com>
2025-01-23 20:39:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const TelegramBot = require('node-telegram-bot-api');
 // Leggi il token dal file .env
 const token = process.env.BOT_TOKEN;
 
-// Inizializza il bot con polling
-const bot = new TelegramBot(token, { polling: true });
+// L'URL del tuo servizio su Render (usa l'URL che Render ti ha fornito)
+const url = 'https://telegram-bot-oplp.onrender.com';
+
+// Inizializza il bot
+const bot = new TelegramBot(token);
 
 // Assicurati che il webhook non venga mai usato
 bot.setWebHook(""); // Disabilita qualsiasi webhook esistente
+
+// Imposta il nuovo webhook
+bot.setWebHook(`${url}/bot${token}`);
 
 // Simula l'ascolto su una porta (anche se non serve per Telegram)
 const port = process.env.PORT || 3000;
