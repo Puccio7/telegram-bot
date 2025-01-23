cfdba9c46a2dfa80723328756744e1eb900dcfd6
Puccio7 <andreapucci2000@gmail.com>
2025-01-23 19:49:45 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// Inserisci il tuo token qui
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  bot.sendMessage(msg.chat.id, `Ciao ${msg.from.first_name}, hai scritto: "${msg.text}"`);
+});
