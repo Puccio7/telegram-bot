8021f9afd99d9c2cfaa0241d7435f4cfeb07ebb7
Puccio7 <andreapucci2000@gmail.com>
2025-01-23 22:52:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
+const express = require('express');
+const app = express();
 
 // Leggi il token dal file .env
 const token = process.env.BOT_TOKEN;
 
 // Inizializza il bot
 const bot = new TelegramBot(token);
-
+// Simula l'ascolto su una porta (anche se non serve per Telegram)
+const port = process.env.PORT || 3000;
 // L'URL del tuo servizio su Render (usa l'URL che Render ti ha fornito)
 const url = 'https://telegram-bot-oplp.onrender.com';
 
 // Imposta il nuovo webhook
 bot.setWebHook(`${url}/bot${token}`);
-
-// Importa i comandi dal file command.js
-const { setupCommands } = require('./command');
-
-// Avvia i comandi per il bot
-setupCommands(bot);
-
-// Simula l'ascolto su una porta (anche se non serve per Telegram)
-const port = process.env.PORT || 3000;
-const express = require('express');
-const app = express();
-
-// Aggiungi un endpoint "di controllo" per Render
-app.get('/', (req, res) => {
-  res.send('Il bot Telegram è attivo!');
-});
 
 // Assicurati di fare il parse del corpo della richiesta
 app.use(express.json());  // Per fare il parse di JSON
@@ -37,7 +24,18 @@ app.post(url, (req, res) => {
   if (message) {
     const chatId = message.chat.id;
     const text = message.text;
-    
+        // Rispondi a un messaggio
+    bot.sendMessage(chatId, `Hai scritto: ${text}`);
+  }
+  res.sendStatus(200); // Rispondi a Telegram con uno status di successo
+});
+
+// Importa i comandi dal file command.js
+const { setupCommands } = require('./command');
+
+// Avvia i comandi per il bot
+setupCommands(bot);
+
 // Avvia il server Express
 app.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);
